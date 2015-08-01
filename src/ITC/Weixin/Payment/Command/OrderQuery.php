a9d1cb8f9b3bc30b51ecd917de1c75655cd938f7
Rich Choy <rich@it-consultis.com>
2015-08-01 09:20:39 +0800

    started pay/orderquery command

---
+++ src/ITC/Weixin/Payment/Command/OrderQuery.php
@@ -0,0 +1,42 @@
+<?php namespace ITC\Weixin\Payment\Command;
+
+/**
+ * see https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=9_2&index=4
+ */
+class OrderQuery extends Command {
+
+    /**
+     * Satisfies ITC\Weixin\Payment\Contracts\Command#name
+     * @param void
+     * @return string
+     */
+    public function name()
+    {
+        return 'pay/orderquery';
+    }
+
+    /**
+     * Overrides ITC\Weixin\Payment\Command\Command#validateParams
+     * @param void
+     * @return array
+     */
+    protected function validateParams(array $params, array &$errors)
+    {
+        parent::validateParams($params, $errors);
+
+        if (empty($params['transaction_id']) && empty($params['out_trade_no']))
+        {
+            $errors[] = 'transaction_id and out_trade_no cannot *both* be empty';
+        }
+    }
+
+    /**
+     * Satisfies ITC\Weixin\Payment\Command\Command#getRequiredParams
+     * @param void
+     * @return array
+     */
+    protected function getRequiredParams()
+    {
+        return [];
+    }
+}
