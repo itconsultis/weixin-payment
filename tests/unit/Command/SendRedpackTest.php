a439cda21ce6025cdda4a127fc574a9278212da1
benjah1 <benjaminhuang1@gmail.com>
2015-12-28 06:47:45 +0000

    add invalid parameter test for command mmpaymkttransfers/sendredpack

--- tests/unit/Command/SendRedpackTest.php
+++ tests/unit/Command/SendRedpackTest.php
@@ -99,4 +99,42 @@ class SendRedpackTest extends TestCase
             }
         }
     }
+
+    public function test_parameter_validation2()
+    {
+        $create_params = function () {
+            return [
+                'mch_billno' => '10000098201411111234567890',
+                'send_name' => '天虹百货',
+                're_openid' => 'oxTWIuGaIt6gTKsQRLau2M0yL16E',
+                'total_amount' => 1000,
+                'total_num' => 1,
+                'client_ip' => '192.168.0.1',
+                'wishing' => '感谢您参加猜灯谜活动，祝您元宵节快乐！',
+                'act_name' => '猜灯谜抢红包活动',
+                'remark' => '猜越多得越多，快来抢！',
+            ];
+        };
+
+        $invalid_params = [
+            'mch_billno' => str_pad('', 33, '0'),
+            'send_name' => str_pad('', 33, '0'),
+            're_openid' => str_pad('', 33, '0'),
+            'wishing' => str_pad('', 129, '0'),
+            'act_name' => str_pad('', 33, '0'),
+            'remark' => str_pad('', 257, '0'),
+        ];
+
+        foreach ($invalid_params as $key => $val) {
+            $params = $create_params();
+            $params[$key] = $val;
+
+            try {
+                $this->command->execute($params);
+                $this->fail();
+            } catch (InvalidArgumentException $e) {
+                continue;
+            }
+        }
+    }
 }
